226109b6f3e13807e35cf4899f5ddad21cbdbae6
Remkooop <936799607@qq.com>
2025-04-15 16:36:23 +0800

    create several entities

---
+++ src/ApplicationManager.java
@@ -0,0 +1,5 @@
+public class ApplicationManager {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/entity/Budget.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class Budget {
+    public String budgetId;
+    public String amount;
+    public String date;
+    public TransactionType type;
+    public User owner;
+}
