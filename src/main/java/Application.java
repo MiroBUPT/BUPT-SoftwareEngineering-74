7b45372a90a4bdc087f419a83c64c17818537ad2
MiroBUPT <jp2022213443@qmul.ac.uk>
2025-04-21 22:26:20 +0800

    修改 application 程序和相关 manager,以保证其能启动软件 GUI

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,14 @@
 import control.ApplicationManager;
+import javax.swing.SwingUtilities;
 
 public class Application {
     public static void main(String[] args) {
         var appManager = ApplicationManager.getInstance();
         appManager.startApp();
+
+        // 启动 GUI 界面
+        SwingUtilities.invokeLater(() -> {
+            boundary.GUIView.main(args);
+        });
     }
 }
